2e34957fbef2b00bc5a6c72c093efc6a90c1ca37
Denis P <6231990@gmail.com>
2026-04-27 11:13:08 +0300

    feat - 4 videos

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -9,4 +9,14 @@ export const videos = [
     thumbnail: "/video/posters/nebo.jpg",
     title: "Нарисуй мне небо"
   },
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-185227231&id=456239607&hash=1ac8559eb7d7c846&hd=3&autoplay=1",
+    thumbnail: "/video/posters/nofear.jpg",
+    title: "Ничего не бойся"
+  },
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-185227231&id=456239614&hash=84e4c6abd25dc89d&hd=3&autoplay=1",
+    thumbnail: "/video/posters/fire.jpg",
+    title: "Ты сделана из огня"
+  },
 ];
