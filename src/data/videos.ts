4e578ca03e18366f6b8cfb35edfec893a9708300
Denis P <6231990@gmail.com>
2026-04-27 10:37:16 +0300

    видео

---
+++ src/data/videos.ts
@@ -0,0 +1,32 @@
+export const videos = [
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-227651167&id=456239034&hash=f926ed73e59ca296&hd=1&autoplay=1",
+    thumbnail: "/video/posters/shtrihi.jpg",
+    title: "Штрихи"
+  },
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-227651167&id=456239034&hash=f926ed73e59ca296&hd=1&autoplay=1",
+    thumbnail: "/video/posters/shtrihi.jpg",
+    title: "Нарисуй мне небо"
+  },
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-227651167&id=456239034&hash=f926ed73e59ca296&hd=1&autoplay=1",
+    thumbnail: "/video/posters/shtrihi.jpg",
+    title: "Выступление 3"
+  },
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-227651167&id=456239034&hash=f926ed73e59ca296&hd=1&autoplay=1",
+    thumbnail: "/video/posters/shtrihi.jpg",
+    title: "Выступление 4"
+  },
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-227651167&id=456239034&hash=f926ed73e59ca296&hd=1&autoplay=1",
+    thumbnail: "/video/posters/shtrihi.jpg",
+    title: "Выступление 5"
+  },
+  {
+    src: "https://vkvideo.ru/video_ext.php?oid=-227651167&id=456239034&hash=f926ed73e59ca296&hd=1&autoplay=1",
+    thumbnail: "/video/posters/shtrihi.jpg",
+    title: "Выступление 6"
+  }
+];
